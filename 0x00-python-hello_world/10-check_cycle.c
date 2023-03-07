6c1b4074121d4ee297b8f31f3653da556c718348
RIvAL-411 <cliffomanyo@gmail.com>
2023-03-07 03:38:15 -0800

    linked list cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+* check_cycle - checks if a singly linked list has a cycle in it
+* @list: first node in the linked list
+*
+* Return: 0 if no cycle found. 1 otherwise
+*/
+int check_cycle(listint_t *list)
+{
+	listint_t *slow = list;
+	listint_t *fast = list;
+
+	if (list == NULL)
+		return ('\0');
+	slow = malloc(sizeof(listint_t));
+	fast = malloc(sizeof(listint_t));
+	if (!slow || !fast)
+		return ('\0');
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			return (1);
+	}
+	free(slow);
+	free(fast);
+
+	return (0);
+}
