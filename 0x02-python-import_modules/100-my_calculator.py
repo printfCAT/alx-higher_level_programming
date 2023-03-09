e8168ceb2c9994bb417a0b053652153f203c9592
RIvAL-411 <cliffomanyo@gmail.com>
2023-03-09 14:58:58 -0800

    calculating my sleep time

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
+    length = len(sys.argv) - 1
+    if length != 3:
+        print("Usage: ./100-my_calculator.py <a>, <operator>, <b>")
+        sys.exit(1)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    operator = sys.argv[2]
+    if operator == '+':
+        calc = add(a, b)
+    elif operator == '-':
+        calc = sub(a, b)
+    elif operator == '*':
+        calc = mul(a, b)
+    elif operator == '/':
+        calc = div(a, b)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+    print("{} {} {} = {}".format(a, operator, b, calc))
