61ca108155def9e99213b0583a2f31fdb886dcc3
RIvAL-411 <cliffomanyo@gmail.com>
2023-03-10 15:35:10 -0800

    print names

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+    """Print all names defined by hidden_4 module."""
     import hidden_4
     for name in dir(hidden_4):
         if name[:2] != "__":
