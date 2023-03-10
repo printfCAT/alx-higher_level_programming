ca3efa80727914f26efbdc89fbacbbe7e576e6ea
RIvAL-411 <cliffomanyo@gmail.com>
2023-03-10 07:31:02 -0800

    calculating my sleep time

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     length = len(sys.argv) - 1
     if length != 3:
-        print("Usage: ./100-my_calculator.py <a>, <operator>, <b>")
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
     a = int(sys.argv[1])
     b = int(sys.argv[3])
