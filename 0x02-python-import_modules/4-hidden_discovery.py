4c622c83ed16878488d09e8ecbaea61f26daee14
RIvAL-411 <cliffomanyo@gmail.com>
2023-03-10 07:50:38 -0800

    print names

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,4 +4,4 @@ if __name__ == "__main__":
     names = dir(hidden_4)
     for name in names:
         if name[0] != '_' and name[1] != '_':
-            print(name)
+            print("{}".format(name))
