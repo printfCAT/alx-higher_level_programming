6ee4e0bdc514accdb537ab6e419556e30642cb97
RIvAL-411 <cliffomanyo@gmail.com>
2023-03-13 05:11:39 -0700

    you cant C me

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    i = 0
+    new_string = ''
+    while i < len(my_string):
+        if my_string[i] != 'C' and my_string[i] != 'c':
+            new_string += my_string[i]
+        i += 1
+    return new_string
