3f5d2012be0ade2a077cb3757eaf8cabafd37b14
RIvAL-411 <cliffomanyo@gmail.com>
2023-03-13 06:14:25 -0700

    print matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in i:
-            print("{:d}".format(j), end=' ')
+            print("{:d}".format(j), end='')
+            if j != len(i):
+                print(" ", end='')
         print()
