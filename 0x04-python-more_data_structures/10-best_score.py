b494c2e389a41d9ad7271654968582df71dab3b9
RIvAL-411 <cliffomanyo@gmail.com>
2023-03-15 09:38:06 -0700

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if not isinstance(a_dictionary, dict):
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
     best_key = list(a_dictionary.keys())[0]
     best_value = a_dictionary[best_key]
@@ -8,4 +8,4 @@ def best_score(a_dictionary):
         if value > best_value:
             best_key = key
             best_value = value
-    return (best_key)
+    return best_key
