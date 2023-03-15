95d11be750a9eb44dfaffb1e4c69b6c6113af087
RIvAL-411 <cliffomanyo@gmail.com>
2023-03-15 07:16:53 -0700

    delete key

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    new_dictionary = dict(a_dictionary)
-    if key in new_dictionary:
-            del new_dictionary[key]
-    return new_dictionary
+    if key in a_dictionary:
+        del a_dictionary[key]
+    return a_dictionary
