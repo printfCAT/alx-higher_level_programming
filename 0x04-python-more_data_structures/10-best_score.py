9706e7178f149d81a930fd7c43d2b8d201d04a2b
RIvAL-411 <cliffomanyo@gmail.com>
2023-03-15 09:34:40 -0700

    best score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not isinstance(a_dictionary, dict):
+        return None
+    best_key = list(a_dictionary.keys())[0]
+    best_value = a_dictionary[best_key]
+    for key, value in a_dictionary.items():
+        if value > best_value:
+            best_key = key
+            best_value = value
+    return best_key
