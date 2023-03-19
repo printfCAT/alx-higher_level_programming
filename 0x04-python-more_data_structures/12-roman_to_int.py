5048b378bc1a089634dd2d96e950ab37daa9222c
RIvAL-411 <cliffomanyo@gmail.com>
2023-03-19 13:46:58 -0700

    roman number and weighted average

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    if not isinstance(roman_string, str):
+        return 0
+    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
+                  'D': 500, 'M': 1000}
     integer = 0
     prev_value = 0
-    
     for char in roman_string[::-1]:
         current_value = roman_dict[char]
         if current_value < prev_value:
@@ -11,5 +13,4 @@ def roman_to_int(roman_string):
         else:
             integer += current_value
         prev_value = current_value
-        
     return integer
