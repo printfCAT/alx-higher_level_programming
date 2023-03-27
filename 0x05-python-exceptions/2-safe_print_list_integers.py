2f59637a063f6f9b14b2deb230f72223a6e9a9c7
printfCAT <cliffomanyo@gmail.com>
2023-03-28 01:09:03 +0300

    print only int

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -12,5 +12,5 @@ def safe_print_list_integers(my_list=[], x=0):
             i += 1
         print()
     except IndexError:
-        print("List index out of range")
+        print("list index out of range")
     return i
