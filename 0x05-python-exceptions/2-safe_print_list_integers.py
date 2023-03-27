3fac6be6bffe98f1f4178afd10b327b54f816bed
printfCAT <cliffomanyo@gmail.com>
2023-03-28 01:38:21 +0300

    print only int

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,15 +2,13 @@
 def safe_print_list_integers(my_list=[], x=0):
     int_list = []
     i = 0
-    for element in my_list[:x]:
-        if isinstance(element, int):
-            element = int(element)
-            int_list.append(element)
     try:
-        for int_element in int_list:
-            print("{:d}".format(int_element), end="")
-            i += 1
+        for element in my_list[:x]:
+            if isinstance(element, int):
+                int_list.append(element)
+                print("{:d}".format(element), end="")
+                i += 1
         print()
-    except (ValueError, TypeError):
+    except IndexError:
         print("list index out of range")
     return i
