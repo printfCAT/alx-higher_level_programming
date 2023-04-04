3b419a9b4b3bb8117c8f42fbb9779e7b53d98bac
RIvAL-411 <cliffomanyo@gmail.com>
2023-04-04 09:19:27 -0700

    delete instance

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -67,5 +67,6 @@ class Rectangle:
     def __repr__(self):
         """returns a string rep of a rectangle"""
         return "Rectangle({}, {})". format(self.__width, self.__height)
+
     def __del__(self):
         print("Bye rectangle...")
