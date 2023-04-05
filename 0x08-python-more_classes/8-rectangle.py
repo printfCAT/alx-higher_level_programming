2141334c59d019e75851562a4cfc5bb487e3f60b
RIvAL-411 <cliffomanyo@gmail.com>
2023-04-05 00:29:08 -0700

    compare rectangle

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -74,6 +74,7 @@ class Rectangle:
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
 
+    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """returns the bigger rectangle"""
         if not isinstance(rect_1, Rectangle):
