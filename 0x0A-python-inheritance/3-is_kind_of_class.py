3bdb0e4c2e057451a8f182c8cf805d56bfe2ad44
RIvAL-411 <cliffomanyo@gmail.com>
2023-04-11 10:37:35 -0700

    is instance subclass or superclass

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" checks instance of object against class and subclass """
+
+
+def is_kind_of_class(obj, a_class):
+    """ the function that does the checking """
+    return isinstance(obj, a_class)
