3ec1831e968b89774bd88157398699ab08e806a2
RIvAL-411 <cliffomanyo@gmail.com>
2023-04-11 12:52:58 -0700

    area exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" define a class BaseGeometry """
+
+
+class BaseGeometry:
+    """ superclass BaseGeometry """
+    pass
+
+    def area(self):
+        """ instance for calculating area """
+        raise Exception("area() is not implemented")
