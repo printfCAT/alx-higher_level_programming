069bef09b0285b2678b8951bf0c7bfe56cbf3a2e
RIvAL-411 <cliffomanyo@gmail.com>
2023-04-13 16:50:35 -0700

    studen to json with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" define a class """
+
+
+class Student:
+    """ superclass Student """
+    def __init__(self, first_name, last_name, age):
+        """
+            Args:
+                first_name(str). student's 1st name
+                last_name(str). student's 2nd name
+                age(int). student's age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ retrieves a dictionary rep of student instance """
+        if isinstance(attrs, list) and all(isinstance(item, str) for item in attrs):
+            return attrs
+        else:
+            return {
+                    "first_name": self.first_name,
+                    "last_name": self.last_name,
+                    "age": self.age
+                    }
