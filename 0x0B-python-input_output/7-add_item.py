af642ab40bcaeb3905d520075f7b370a2deac05d
RIvAL-411 <cliffomanyo@gmail.com>
2023-04-13 14:44:31 -0700

    load add save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+""" load, add and save to file """
+
+
 import sys
 load = __import__('6-load_from_json_file').load_from_json_file
 save = __import__('5-save_to_json_file').save_to_json_file
