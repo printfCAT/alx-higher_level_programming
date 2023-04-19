ddfdc5783ee5b5e6bd6d6df8fc5824ac219cc2c1
printfCAT <117769903+printfCAT@users.noreply.github.com>
2023-04-19 09:07:12 +0300

    Update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -44,7 +44,7 @@ class Base:
     @staticmethod
     def from_json_string(json_string):
         """ returns list of JSON string rep """
-        if json_string is None:
+        if json_string is None or json_string == "[]":
             return "[]"
         else:
             return json.loads(json_string)
