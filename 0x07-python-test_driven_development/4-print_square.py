eebf0aee8885bdc2b6ce970043a20118311b2143
RIvAL-411 <cliffomanyo@gmail.com>
2023-05-01 01:01:00 -0700

    print square test

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" define a function """
+
+
+def print_square(size):
+    """ prints a square using # """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(0, size):
+        [print("#", end="") for j in range(size)]
+        print("")
+    if size == 0:
+        print("")
