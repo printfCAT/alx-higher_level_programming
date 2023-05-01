396eab927357e78ff301b9999b6c895a124b86dd
RIvAL-411 <cliffomanyo@gmail.com>
2023-05-01 06:57:12 -0700

    text indentation test

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" define a function """
+
+
+def text_indentation(text):
+    """ prints lines of text inserting newlines after ., ? and : """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    result = ""
+    add_newline = False
+    for char in text:
+        if add_newline:
+            result += "\n\n"
+            add_newline = False
+        if char == " " and not add_newline and (result == "" or result[-1] == "\n"):
+            continue
+        result += char
+        if char in ".?:":
+            add_newline = True
+    if add_newline:
+        result += "\n\n"
+    print(result.rstrip())
