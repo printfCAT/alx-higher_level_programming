7775959c1888bd606f2070c5834d5584588bbd05
RIvAL-411 <cliffomanyo@gmail.com>
2023-05-01 01:08:26 -0700

    print square correction

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -9,5 +9,8 @@ def print_square(size):
     if size < 0:
         raise ValueError("size must be >= 0")
     for i in range(size):
-        [print("#", end="") for j in range(size)]
-        print("")
+        if size == 0:
+            print("")
+        else:
+            [print("#", end="") for j in range(size)]
+            print("")
