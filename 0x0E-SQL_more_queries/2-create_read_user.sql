b4fa6fbeab10ca9147e879b03af4a45080f29e4e
RIvAL-411 <cliffomanyo@gmail.com>
2023-06-02 13:23:24 -0700

    read user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,10 +1,10 @@
 -- creates database hbtn_0d_2 and the user user_0d_2
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER
-    IF NOT EXISTS 'user_0d_2'@'hbtn_0d_2'
+    IF NOT EXISTS 'user_0d_2'@'localhost'
     IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT
-    ON *.*
-    TO 'user_0d_2'@'hbtn_0d_2'
+    ON 'hbtn_0d_2'.*
+    TO 'user_0d_2'@'localhost'
     IDENTIFIED BY 'user_0d_2_pwd';
 FLUSH PRIVILEGES;
