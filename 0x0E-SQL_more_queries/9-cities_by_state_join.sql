b56cf43b103cbac6842708ccd5a3ffbc65b8bba1
RIvAL-411 <cliffomanyo@gmail.com>
2023-06-03 07:00:42 -0700

    Cities by States

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- lists all cities contained in the database hbtn_0d_usa
 SELECT cities.id, cities.name, states.name FROM states
-JOIN cities ON cities.id = states.id
+INNER JOIN cities ON cities.id = states.id
 ORDER BY cities.id ASC;
