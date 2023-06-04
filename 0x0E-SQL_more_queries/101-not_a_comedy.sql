ab1b39f3beb03067e7338650fad6b78296b8b489
RIvAL-411 <cliffomanyo@gmail.com>
2023-06-04 05:05:37 -0700

    No Comedy tonight!

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,5 +1,5 @@
 -- lists all shows without the genre Comedy in the database hbtn_0d_tvshows
-SELECT tv_shows.title AS title FROM tv_shows
+SELECT DISTINCT tv_shows.title AS title FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
 LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
 WHERE tv_shows.title NOT IN (
