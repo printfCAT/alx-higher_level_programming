0a988221ea5bf0ddfb36934673af981cae9ae986
RIvAL-411 <cliffomanyo@gmail.com>
2023-06-12 12:46:40 -0700

    Value of my argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -3,9 +3,5 @@ const { argv } = require('process');
 if (argv[2] === undefined) {
   console.log('No argument');
 } else {
-  let i = 2;
-  while (argv[i] !== undefined) {
-    console.log(argv[i]);
-    i++;
-  }
+  console.log(argv[2]);
 }
