5b5c45410a5152215bc8239b01cf483d4ba72637
RIvAL-411 <cliffomanyo@gmail.com>
2023-06-12 15:35:21 -0700

    Second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -7,11 +7,13 @@ if (argv[2] === undefined || argv.length === 2) {
 } else {
   for (let i = 2; i < argv.length; i++) {
     const current = argv[i];
-    if (current > bigone) {
-      bigtwo = bigone;
-      bigone = current;
-    } else if (current > bigtwo && current < bigone) {
-      bigtwo = current;
+    if (!isNaN(current)) {
+      if (current > bigone) {
+        bigtwo = bigone;
+        bigone = current;
+      } else if (current > bigtwo && current < bigone) {
+        bigtwo = current;
+      }
     }
   }
   console.log(bigtwo);
