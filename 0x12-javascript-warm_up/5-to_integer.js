8464249d282a37a82f03aff15630114e0deb1426
RIvAL-411 <cliffomanyo@gmail.com>
2023-06-12 13:09:03 -0700

    An integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 const { argv } = require('process');
-if (parseInt(argv[2], 10) === parseInt(argv[2], 10)) {
+const input = parseInt(argv[2], 10);
+if (input === parseInt(argv[2], 10)) {
   console.log('My number:', argv[2]);
 } else {
   console.log('Not a number');
