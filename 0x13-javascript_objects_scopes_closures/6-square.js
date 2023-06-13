66bba0a3fc63070f33da3489a07d9e3ba51ec15f
RIvAL-411 <cliffomanyo@gmail.com>
2023-06-13 13:53:35 -0700

    task 6 correction

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,16 +2,16 @@
 const Rectangle = require('./5-square');
 module.exports = class Square extends Rectangle {
   charPrint (c) {
-    for (let i = 0; i < this.height; i++) {
-      let square = '';
-      for (let j = 0; j < this.height; j++) {
-        if (c === undefined) {
-          square += 'X';
-        } else {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        let square = '';
+        for (let j = 0; j < this.width; j++) {
           square += 'C';
         }
+        console.log(square);
       }
-      console.log(square);
     }
   }
 };
