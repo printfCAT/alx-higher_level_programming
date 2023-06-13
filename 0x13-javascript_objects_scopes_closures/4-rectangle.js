6ca67e8335819838d893432d6f264d1b4b55b8cd
RIvAL-411 <cliffomanyo@gmail.com>
2023-06-13 12:09:20 -0700

    Rectangle #4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let rectangle = '';
+      for (let j = 0; j < this.width; j++) {
+        rectangle += 'X';
+      }
+      console.log(rectangle);
+    }
+  }
+
+  rotate () {
+    this.width = this.height;
+    this.height = this.width;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+};
