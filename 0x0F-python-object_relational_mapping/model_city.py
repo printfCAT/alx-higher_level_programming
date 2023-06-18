a7b42b39142b8859f1715f4704751284d26a2a53
RIvAL-411 <cliffomanyo@gmail.com>
2023-06-18 15:05:06 -0700

    task 14

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -2,11 +2,11 @@
 """ define a subclass City """
 
 
+from model_state import Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
-Base = declarative_base()
 
 class City(Base):
     """ subclass City """
