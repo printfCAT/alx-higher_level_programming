fae4753734c4c42d68b340f9389100cd4a9e7a49
RIvAL-411 <cliffomanyo@gmail.com>
2023-06-18 05:12:07 -0700

    Contains 'a'

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" define subclass State """
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State
+from sys import argv
+
+if __name__ == "__main__":
+    engine = create_engine(f'mysql://{argv[1]}:{argv[2]}\
+                            @localhost:3306/{argv[3]}')
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).order_by(State.id.asc())
+    for state in states:
+        if 'a' in state.name:
+            print("{}: {}".format(state.id, state.name))
+        else:
+            continue
