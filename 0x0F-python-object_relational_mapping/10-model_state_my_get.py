7a56064056a1a6edbe832cb5399998fbc13e0338
RIvAL-411 <cliffomanyo@gmail.com>
2023-06-18 05:41:51 -0700

    Get a state

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" define subclass State """
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State
+from sys import argv
+
+if __name__ == "__main__":
+    engine = create_engine(f'mysql://{argv[1]}:{argv[2]}\
+                            @localhost:3306/{argv[3]}')
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).order_by(State.id.asc())
+    match_found = False
+    for state in states:
+        if state.name == argv[4]:
+            print(state.id)
+            Match_found = True
+    if not Match_found:
+        print("Not found")
