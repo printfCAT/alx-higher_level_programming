666024179e1753240f1ff741284c94ab4e9fcbcf
RIvAL-411 <cliffomanyo@gmail.com>
2023-06-18 04:45:11 -0700

    all states via SQLAlchemy

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" define subclass State """
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State
+from sys import argv
+
+if __name__ == "__main__":
+    engine = create_engine(f'mysql://{argv[1]}:{argv[2]}\
+                            @localhost:3306/{argv[3]}')
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).order_by(State.id.asc()).first()
+    print("{}: {}".format(states.id, states.name))
