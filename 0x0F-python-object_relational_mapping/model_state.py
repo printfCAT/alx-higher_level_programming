9872c1f3dd1403bd200ee4619762c27cdb7723c6
RIvAL-411 <cliffomanyo@gmail.com>
2023-06-17 23:39:09 -0700

    First state model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,17 +2,9 @@
 """ define a subclass State """
 
 
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from sys import argv
 
-
-engine = create_engine(f'mysql://{argv[1]}:{argv[2]}@localhost:3306/')
-engine.execute(f"CREATE DATABASE IF NOT EXISTS {argv[3]}")
-engine = create_engine(f'mysql://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}')
-Session = sessionmaker(bind=engine)
-session = Session()
 Base = declarative_base()
 
 
@@ -22,6 +14,3 @@ class State(Base):
     id = Column(Integer, autoincrement=True, unique=True,
                 nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-
-
-Base.metadata.create_all(engine)
