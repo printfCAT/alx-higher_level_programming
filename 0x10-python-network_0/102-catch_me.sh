ce8fa1c992790e60fd7f949afcb293ecaca877e5
RIvAL-411 <cliffomanyo@gmail.com>
2023-07-02 14:09:51 -0700

    task 9 fix

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a request to a server that responds with a message
-curl -s -X PUT -H "Origin: You got me!" 0.0.0.0:5000/catch_me
+curl -sL -X PUT -H "Origin: You got me!" 0.0.0.0:5000/catch_me
