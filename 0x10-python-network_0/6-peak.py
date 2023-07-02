abfed3f8530b5cca363f736db8868252874b4d23
RIvAL-411 <cliffomanyo@gmail.com>
2023-07-02 12:44:27 -0700

    Find a peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def find_peak(list_of_integers):
+    peaks = []
+    if len(list_of_integers) < 1:
+        return None
+    elif len(set(list_of_integers)) == 1:
+        return str(list_of_integers[0])
+    elif len(list_of_integers) == 1:
+        peaks.append(str(list_of_integers[0]))
+    else:
+        for i in range(len(list_of_integers) - 1):
+            if i == 0:
+                if list_of_integers[i] > list_of_integers[i + 1]:
+                    peaks.append(str(list_of_integers[i]))
+            elif i == len(list_of_integers) - 1:
+                if list_of_integers[i] > list_of_integers[i - 1]:
+                    peaks.append(str(list_of_integers[i]))
+            else:
+                if list_of_integers[i] > list_of_integers[i - 1] and list_of_integers[i] > list_of_integers[i + 1]:
+                    peaks.append(str(list_of_integers[i]))
+    return ", ".join(peaks)
