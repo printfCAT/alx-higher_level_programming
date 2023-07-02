f03c6a42c8ecd3ec45f0bf416c1e0e3ce7272861
RIvAL-411 <cliffomanyo@gmail.com>
2023-07-02 13:10:14 -0700

    task 6 refix

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,22 +1,15 @@
 #!/usr/bin/python3
+""" sorting list algorithm """
+
+
 def find_peak(list_of_integers):
-    peaks = []
-    if len(list_of_integers) < 1:
+    """ returns a peak in a list of unsorted integers """
+    if not list_of_integers:
         return None
-    elif len(set(list_of_integers)) == 1:
-        return str(list_of_integers[0])
-    elif len(list_of_integers) == 1:
-        peaks.append(str(list_of_integers[0]))
-    else:
-        for i in range(len(list_of_integers) - 1):
-            if i == 0:
-                if list_of_integers[i] > list_of_integers[i + 1]:
-                    peaks.append(str(list_of_integers[i]))
-            elif i == len(list_of_integers) - 1:
-                if list_of_integers[i] > list_of_integers[i - 1]:
-                    peaks.append(str(list_of_integers[i]))
-            else:
-                if list_of_integers[i] > list_of_integers[i - 1] \
-                   and list_of_integers[i] > list_of_integers[i + 1]:
-                    peaks.append(str(list_of_integers[i]))
-    return ", ".join(peaks)
+    peak = list_of_integers[0]
+
+    for num in list_of_integers:
+        if num > peak:
+            peak = num
+
+    return peak
