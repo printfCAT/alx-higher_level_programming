0129161806dc0b61bbe12f102675b4c746bf0dd0
RIvAL-411 <cliffomanyo@gmail.com>
2023-07-04 07:51:28 -0700

    task 0 fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,4 +5,8 @@
 import urllib.request
 
 with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    print(response.read())
+    body = response.read()
+    print("Body response:")
+    print("\t- type:", type(body))
+    print("\t- content:", body)
+    print("\t- utf8 content:", body.decode('utf-8'))
