c19a63e088bae639428701bf5b2564f7dea1f4e4
RIvAL-411 <cliffomanyo@gmail.com>
2023-07-04 09:05:42 -0700

    POST an email #0

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,7 +9,7 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
-    values = {'email' : email}
+    values = {'email': email}
     data = urllib.parse.urlencode(values)
     data = data.encode('ascii')
     req = urllib.request.Request(url, data)
