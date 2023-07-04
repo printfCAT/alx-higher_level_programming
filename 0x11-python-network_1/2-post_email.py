f4de1a2c163eab70ae06c4718eadcbd7aa2f09e5
RIvAL-411 <cliffomanyo@gmail.com>
2023-07-04 09:04:33 -0700

    POST an email #0

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" define a python script """
+
+
+import urllib.request
+import urllib.parse
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    values = {'email' : email}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        page = response.read()
+    print(page.decode('utf-8'))
