4d15b65d484441d210bbd9c2bb19b0a83a60614d
RIvAL-411 <cliffomanyo@gmail.com>
2023-07-04 08:31:56 -0700

    task 1 fix

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,6 +6,7 @@ import urllib.request
 import sys
 
 url = sys.argv[1]
-with urllib.request.urlopen(url) as response:
+req = urllib.request.Request(url)
+with urllib.request.urlopen(req) as response:
     x_request_id = response.headers.get('X-Request-Id')
 print(x_request_id)
